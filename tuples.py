769149e909aaf5098af673b64dd5abb208f056b7
franlu0 <162721582+franlu0@users.noreply.github.com>
2024-05-24 12:22:16 -0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,32 +1,18 @@
-"""Functions to help Azara and Rui locate pirate treasure."""
-
-
 def get_coordinate(record):
-    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
-
-    :param record: tuple - with a (treasure, coordinate) pair.
-    :return: str - the extracted map coordinate.
-    """
-
-    return ""
-
+    teasure, coordinate=record
+    return coordinate
 
 def convert_coordinate(coordinate):
-    """Split the given coordinate into tuple containing its individual components.
-
-    :param coordinate: str - a string map coordinate
-    :return: tuple - the string coordinate split into its individual components.
-    """
-
-    return ()
+    num, letra=coordinate
+    return num, letra
 
 
 def create_record(azara_record, rui_record):
-    """Combine the two record types (if possible) and create a combined record group.
-
-    :param azara_record: tuple - a (treasure, coordinate) pair.
-    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
-    :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
-    """
-
-    return ()
+    tesoro, coordenadaA=azara_record
+    ubicación, coordenadaR, cuadrante=rui_record
+    NUM,LETRA=coordenadaA
+    conjunto= NUM,LETRA
+    if conjunto==coordenadaR:
+        return tesoro, coordenadaA, ubicación, coordenadaR, cuadrante
+    else:
+        return "not a match"   
